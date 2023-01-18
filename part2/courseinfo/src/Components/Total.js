d1f8e63018a8de8df94e9f5b77914cedbd2f42c4
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2023-01-18 21:13:57 +0000

    completed ex 2.2

---
+++ part2/courseinfo/src/Components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = ({ course }) => {
+
+    const total = course.parts.reduce((acc, part) => acc + part.exercises, 0)
+    return (
+        <div>Total exercises: {total}</div>
+    )
+}
+
+export default Total
